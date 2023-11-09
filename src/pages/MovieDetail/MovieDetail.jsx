49944a09182a977ba19b747763fa36c0ec2293fa
SETI108 <seti108@gmail.com>
2023-11-09 22:15:35 +0100

    Add styling

--- src/pages/MovieDetail/MovieDetail.jsx
+++ src/pages/MovieDetail/MovieDetail.jsx
@@ -1,29 +1,35 @@
 import { useParams, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { ErrorPage } from "../ErrorPage";
 import "./MovieDetail.css";
 
 export const MovieDetail = () => {
   const [movieDetails, setMovieDetails] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [errorState, setErrorState] = useState(false);
   const { movieId } = useParams();
   const apiKey = "d14980dd8df22d55a4bf4592f082a8c6";
   const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=en-US`;
-
+  const viewport = window.innerWidth;
   const imageUrl = `https://image.tmdb.org/t/p/`;
-  const imageBig = "w1280";
-  const fullImage = imageUrl + imageBig;
-  // const rating = parseFloat(movieDetails.vote_average).toFixed(1);
-  // console.log(rating);
+  const imageMedium = "w1280";
+  const imageSmall = "w780";
+
+  const adaptedImage = () => {
+    if (viewport < 768) return imageSmall;
+    else return imageMedium;
+  };
+  const fullImage = imageUrl + adaptedImage(viewport);
 
   const fetchMovieDetails = () => {
     fetch(url)
-      .then((res) => res.json())
+      .then((res) => (res.ok ? res.json() : setErrorState(true)))
       .then((data) => {
         if (data) {
           setMovieDetails(data);
           setIsLoading(false);
         } else {
-          throw new Error("Couldn't contact the API");
+          throw new Error("Error fetching the API");
         }
       })
       .catch((err) => {
@@ -35,12 +41,10 @@ export const MovieDetail = () => {
     fetchMovieDetails();
   }, []);
 
-  console.log(movieDetails);
-
   return (
     <>
       {isLoading ? (
-        <p>Page loading...</p>
+        !errorState && <p>Page loading...</p>
       ) : (
         <section className="movie-section">
           <div className="movie-info">
@@ -50,24 +54,24 @@ export const MovieDetail = () => {
                 Back
               </Link>
             </span>
-            <h1>{movieDetails.title}</h1>
+            <h1>{movieDetails?.title}</h1>
             <div className="wrapper-movie-info">
               <span className="rating">
                 <img src="../../src/assets/star-icon.svg" alt="star icon" />
-                <p>{parseFloat(movieDetails.vote_average).toFixed(1)}</p>
+                <p>{parseFloat(movieDetails?.vote_average).toFixed(1)}</p>
               </span>
               <span className="time">
                 <img src="../../src/assets/clock-icon.svg" alt="star icon" />
-                <span>{movieDetails.runtime}</span>
+                <span>{movieDetails?.runtime}</span>
               </span>
               <span className="time">
                 <img src="../../src/assets/calendar-icon.svg" alt="star icon" />
-                <p>{movieDetails.release_date}</p>
+                <p>{movieDetails?.release_date}</p>
               </span>
             </div>
-            <p>{movieDetails.overview}</p>
+            <p>{movieDetails?.overview}</p>
             <button className="movie-detail-cta">
-              <a href={movieDetails.homepage}>
+              <a href={movieDetails?.homepage}>
                 <span>Learn More</span>
                 <img
                   src="../../src/assets/chevron_right_icon.svg"
@@ -78,10 +82,12 @@ export const MovieDetail = () => {
           </div>
 
           <div className="movie-img">
-            <img src={fullImage + movieDetails.backdrop_path}></img>
+            <img src={fullImage + movieDetails?.backdrop_path}></img>
           </div>
         </section>
       )}
+
+      {errorState && <ErrorPage />}
     </>
   );
 };
