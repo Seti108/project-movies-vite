98557ae0ea924a268fab17234c50be4bde33f667
SETI108 <seti108@gmail.com>
2023-11-09 18:38:25 +0100

    Add CSS classes

--- src/pages/MovieDetail/MovieDetail.jsx
+++ src/pages/MovieDetail/MovieDetail.jsx
@@ -1,6 +1,6 @@
 import { useParams, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
-
+import "./MovieDetail.css";
 
 export const MovieDetail = () => {
   const [movieDetails, setMovieDetails] = useState(null);
@@ -8,7 +8,7 @@ export const MovieDetail = () => {
   const { movieId } = useParams();
   const apiKey = "d14980dd8df22d55a4bf4592f082a8c6";
   const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=en-US`;
-  
+
   const imageUrl = `https://image.tmdb.org/t/p/`;
   const imageBig = "w1280";
   const fullImage = imageUrl + imageBig;
@@ -17,51 +17,71 @@ export const MovieDetail = () => {
 
   const fetchMovieDetails = () => {
     fetch(url)
-    .then((res) => res.json()) 
-    .then((data) => {
-      if(data) {
-        setMovieDetails(data);
-        setIsLoading(false)
-      } else {
-        throw new Error("Couldn't contact the API")
-      }
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-  }
-  
+      .then((res) => res.json())
+      .then((data) => {
+        if (data) {
+          setMovieDetails(data);
+          setIsLoading(false);
+        } else {
+          throw new Error("Couldn't contact the API");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   useEffect(() => {
-    fetchMovieDetails()
-  },[])
-  
+    fetchMovieDetails();
+  }, []);
 
-  console.log(movieDetails)
+  console.log(movieDetails);
 
   return (
     <>
-    {isLoading ?
-    <p>Page loading...</p>:
-    <section>
-    <div className="movie-info">
-      <Link to="/">Back</Link>
-      <h1>{movieDetails.title}</h1>
-        <div className="wrapper-movie-info">
-          <span className="rating"><img src="../../src/assets/star-icon.svg" alt="star icon" /><p>{parseFloat(movieDetails.vote_average).toFixed(1)}</p></span>
-          <span className="time"><img src="../../src/assets/clock-icon.svg" alt="star icon" /><p>{movieDetails.runtime}</p></span>
-          <span className="time"><img src="../../src/assets/calendar-icon.svg" alt="star icon" /><p>{movieDetails.release_date}</p></span>
-        </div>
-      <p>{movieDetails.overview}</p>
-      <a href={movieDetails.homepage}>Learn More</a>
-    </div>
+      {isLoading ? (
+        <p>Page loading...</p>
+      ) : (
+        <section className="movie-section">
+          <div className="movie-info">
+            <span className="back-link">
+              <Link to="/">
+                <img src="../src/assets/chevron_left_icon.svg" alt="" />
+                Back
+              </Link>
+            </span>
+            <h1>{movieDetails.title}</h1>
+            <div className="wrapper-movie-info">
+              <span className="rating">
+                <img src="../../src/assets/star-icon.svg" alt="star icon" />
+                <p>{parseFloat(movieDetails.vote_average).toFixed(1)}</p>
+              </span>
+              <span className="time">
+                <img src="../../src/assets/clock-icon.svg" alt="star icon" />
+                <span>{movieDetails.runtime}</span>
+              </span>
+              <span className="time">
+                <img src="../../src/assets/calendar-icon.svg" alt="star icon" />
+                <p>{movieDetails.release_date}</p>
+              </span>
+            </div>
+            <p>{movieDetails.overview}</p>
+            <button className="movie-detail-cta">
+              <a href={movieDetails.homepage}>
+                <span>Learn More</span>
+                <img
+                  src="../../src/assets/chevron_right_icon.svg"
+                  alt="Visit movie website"
+                ></img>
+              </a>
+            </button>
+          </div>
 
-    <div className="movie-img">
-      <img src={fullImage + movieDetails.backdrop_path}></img>
-    </div>
-
-  </section>
-}
-  </>
-  )
+          <div className="movie-img">
+            <img src={fullImage + movieDetails.backdrop_path}></img>
+          </div>
+        </section>
+      )}
+    </>
+  );
 };
